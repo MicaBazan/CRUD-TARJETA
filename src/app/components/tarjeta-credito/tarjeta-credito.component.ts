d51c6c1b1a7320d9e488d579c24059f9ee2f2b70
MicaBazan <micabazan5@gmail.com>
2024-01-16 18:27:58 -0300

    Listado de tarjetas front listo

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tarjeta-credito',
   templateUrl: './tarjeta-credito.component.html',
   styleUrl: './tarjeta-credito.component.css'
 })
-export class TarjetaCreditoComponent {
 
+export class TarjetaCreditoComponent{
+  listTarjetas: any[] = [
+    { titular: 'Juan Perez', numeroTarjeta: '25251565464', fechaExpiracion: '11/23', cvv: '123'},
+    { titular: 'Miguel Gonzalez', numeroTarjeta: '98769565464', fechaExpiracion: '11/24', cvv: '312'}
+  ];
 }
