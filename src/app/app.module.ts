62fce69d087c8f7e416dae7ff7b14183e36b9933
MicaBazan <micabazan5@gmail.com>
2024-01-16 13:36:32 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarjetaCreditoComponent } from './components/tarjeta-credito/tarjeta-credito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetaCreditoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-credito',
+  templateUrl: './tarjeta-credito.component.html',
+  styleUrl: './tarjeta-credito.component.css'
+})
+export class TarjetaCreditoComponent {
+
+}
